0adbe26ac3338d59bcde513585de228d9994e82a
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-02-01 15:04:19 +0545

    Added Firebase Services for Web

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_app/responsive/mobilescreen_layout.dart';
 import 'package:instagram_app/responsive/responsive_screen_layout.dart';
 import 'package:instagram_app/responsive/webscreen_layout.dart';
 import 'package:instagram_app/utils/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: "AIzaSyD04TYwgaJ9TMccQWQWXV0xYiSBxn-lBrs",
+      appId: "1:7238091765:web:6c9fb905f5ea075c3616bc",
+      messagingSenderId: "7238091765",
+      projectId: "instagram-clone-8acc8",
+      storageBucket: "instagram-clone-8acc8.appspot.com",
+    ));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
