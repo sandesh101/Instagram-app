5ccdbb60060555727c0c8ac3f6447832e823ccc1
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-02-02 18:34:15 +0545

    Added image to firebase storage

--- lib/resources/auth_method.dart
+++ lib/resources/auth_method.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_app/resources/storage_method.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -18,15 +19,15 @@ class AuthMethods {
     String res = "Some error";
 
     try {
-      if (email.isNotEmpty ||
-          password.isNotEmpty ||
-          username.isNotEmpty ||
-          file != null) {
+      if (email.isNotEmpty || password.isNotEmpty || username.isNotEmpty) {
         //register user
         UserCredential cred = await _auth.createUserWithEmailAndPassword(
           email: email,
           password: password,
         );
+
+        String photoUrl = await StorageMethod()
+            .uploadImageStorage('profilePics', file, false);
         //add user to database
         await _firestore.collection('users').doc(cred.user!.uid).set({
           'username': username,
@@ -34,6 +35,7 @@ class AuthMethods {
           'email': email,
           'followers': [],
           'following': [],
+          'photoUrl': photoUrl,
         });
         res = "Success";
       }
