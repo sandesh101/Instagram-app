73930223f3c835f1b4e976bae8522ca3fad6cda4
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-02-02 20:05:02 +0545

    Managed Navigation through StreamBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_app/responsive/mobilescreen_layout.dart';
@@ -36,12 +37,30 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackground,
       ),
-      home: const Scaffold(
-        // body: ResponsiveLayout(
-        //   webScreenLayout: WebScreenLayout(),
-        //   mobileScreenLayout: MobileScreenLayout(),
-        // ),
-        body: LoginScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            if (snapshot.hasData) {
+              return const ResponsiveLayout(
+                webScreenLayout: WebScreenLayout(),
+                mobileScreenLayout: MobileScreenLayout(),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('${snapshot.error}'),
+              );
+            }
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: blueColor,
+              ),
+            );
+          }
+          return const LoginScreen();
+        },
       ),
     );
   }
