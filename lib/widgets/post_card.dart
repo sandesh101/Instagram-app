249aa847b111d5e36e26b52153411887c149248a
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-02-06 21:43:54 +0545

    Added Dialog Box for deleting

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -37,7 +37,28 @@ class PostCard extends StatelessWidget {
                   ),
                 ),
                 IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) => Dialog(
+                                child: ListView(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 16,
+                              ),
+                              shrinkWrap: true,
+                              children: ['Delete']
+                                  .map(
+                                    (e) => InkWell(
+                                      child: Container(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 12, horizontal: 16),
+                                        child: Text(e),
+                                      ),
+                                    ),
+                                  )
+                                  .toList(),
+                            )));
+                  },
                   icon: const Icon(Icons.more_vert),
                 )
               ],
