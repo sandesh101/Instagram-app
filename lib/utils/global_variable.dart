313d549fe8bf46bbb46450e0c9ed06e868b301e9
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-02-06 20:55:13 +0545

    Added feed screen to variable folder

--- lib/utils/global_variable.dart
+++ lib/utils/global_variable.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_app/screens/add_post_screen.dart';
+import 'package:instagram_app/screens/feed_screen.dart';
 
 const webScreen = 600;
 
 const homeScreenItems = [
-  Text('Home'),
+  FeedScreen(),
   Text('Search'),
   AddPost(),
   Text('Nofi'),
