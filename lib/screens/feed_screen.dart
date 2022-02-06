a4b7562c699259abe2be9ddc736a1e76cf2f9aa7
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-02-06 21:03:00 +0545

    Made appbar for homescreen

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_app/utils/colors.dart';
 
 class FeedScreen extends StatelessWidget {
   const FeedScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackground,
+        centerTitle: false,
+        title: SvgPicture.asset(
+          'assets/instagramlogo.svg',
+          color: primaryColor,
+          height: 32,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.chat_rounded),
+          ),
+        ],
+      ),
+    );
   }
 }
