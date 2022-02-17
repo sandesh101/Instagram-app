a1cbe69deadc300f61c2b1ab02d09f6ce31b3ccb
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-02-17 21:03:45 +0545

    Added Column To Show Stats

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -16,6 +16,54 @@ class _ProfileScreenState extends State<ProfileScreen> {
         backgroundColor: mobileBackground,
         title: Text('Username'),
       ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Colors.grey,
+                      backgroundImage: NetworkImage(
+                          'https://images.unsplash.com/photo-1644945583064-e4906134ae14?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'),
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  //Creating the function for shwoing stat column
+  Column buildStatColumn(int num, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          num.toString(),
+          style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.w400,
+            color: Colors.grey,
+          ),
+        ),
+      ],
     );
   }
 }
